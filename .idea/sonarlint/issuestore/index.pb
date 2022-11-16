3d81d837b7b88a64314f860ac53f68055f4006dc
Prof. Jonathan <jonathan.victorica@digitalhouse.com>
2022-11-16 10:39:03 -0300

    changes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,7 +5,13 @@ K
 configData/eurekaServer.yml,9\9\99ce5e0ba025c7e236ee317cd09a384c8efabe42
 F
 configData/gateway.yml,9\1\9105884a137d9a78beafdfed360139b19d73208f
-_
-/configServer/src/main/resources/application.yml,b\2\b28eac76046745705fc5f41eab2d4a9ca8038b9f
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+I
+docker/docker-compose.yml,3\f\3fe733692645251b38d6ef0a55b2d48c3a1cf387
+?
+gateway/pom.xml,4\b\4b316bc60d1cfae51045f6fc8e58a8ae31f03ff3
+E
+testing/students.http,1\5\1582a8804e463f36b446caf7f3f132e8e8ddddee
+_
+/configServer/src/main/resources/application.yml,b\2\b28eac76046745705fc5f41eab2d4a9ca8038b9f
--- api-course/src/main/java/com/dh/course/client/StudentFeign.java
+++ api-course/src/main/java/com/dh/course/client/StudentFeign.java
@@ -1,5 +1,7 @@
 package com.dh.course.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "api-student")
 public interface StudentFeign {
-
+    @CircuitBreaker(name = "clientStudent", fallbackMethod = "getByIdFallback")
+    @Retry(name = "clientStudent")
     @GetMapping("/api/v1/students/{id}")
     Student getById(@PathVariable Long id);
+
+    default Student getByIdFallback(@PathVariable Long id){
+        return null;
+    }
 
     @Getter
     @Setter
